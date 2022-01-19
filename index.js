f51601792cf2c815de85cfe9a82d31b2ee09aac1
jk27 <jcarlosmartin27@gmail.com>
2022-01-19 12:40:51 +0000

    created modules

--- index.js
+++ index.js
@@ -2,49 +2,15 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-///////////////////////////////////////////////////////////////////// FILES
-// BLOCKING SYNCHRONOUS WAY
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-
-// const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File written!");
-
-// NON-BLOCKING SYNCHRONOUS WAY
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-// 	if (err) return console.log("Error!💥");
-// 	fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-// 		console.log(data2);
-// 		fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-// 			console.log(data3);
-// 			fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", err => {
-// 				console.log("Your file has been written! 😊😊😊");
-// 			});
-// 		});
-// 	});
-// });
-// console.log("Will read file!");
+///////////////////////////////////////////////////////////////////// CUSTOM MODULES
+const replaceTemplate = require("./modules/replaceTemplate");
 
 ///////////////////////////////////////////////////////////////////// SERVER
-// DOES => Replaces all placeholders in templates with appropiate data
-const replaceTemplate = (temp, product) => {
-	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-	output = output.replace(/{%ID%}/g, product.id);
 
-	if (!product.organic)
-		output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-	return output;
-};
-
-const tempOverview = fs.readFileSync(`${__dirname}/index.html`, "utf-8");
+const tempOverview = fs.readFileSync(
+	`${__dirname}/templates/index.html`,
+	"utf-8"
+);
 const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8");
 const tempProduct = fs.readFileSync(
 	`${__dirname}/templates/product.html`,
